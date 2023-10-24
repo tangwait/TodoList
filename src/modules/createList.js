0c385908a0019ffa51de1242e5f96e29ebd9065c
tangwai <tmichael11314@gmail.com>
2023-10-24 06:39:37 -0500

    initial commit add createList/addToList > console.log

---
+++ src/modules/createList.js
@@ -0,0 +1,19 @@
+
+export function createList(name, desc, dueDate) {
+    const list = {
+        name, 
+        desc, 
+        dueDate
+    };
+    console.log("createList works!")
+    return list
+}
+
+export function addToList(name, desc, dueDate) {
+    console.log("addtoList works")
+}
+
+function listPriority() {
+
+}
+

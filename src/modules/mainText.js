efbaaf809a35c8359306995e780d7de336aa3c01
tangwai <tmichael11314@gmail.com>
2023-11-03 02:29:36 -0500

    add change texts for relevant texts simplify change header() add change form texts add logo image

--- src/modules/mainText.js
+++ src/modules/mainText.js
@@ -1,52 +1,43 @@
-
-// export function changeHeaderLogo() {
-//     const headerLogo = document.querySelector('.headerLogo');
-//     headerLogo.textContent = 'This is the header logo'
-//     console.log('this works');
-// }
-
-// export function changeHeaderTitle() {
-//     const headerTitle = document.querySelector('.headerTitle');
-//     headerTitle.textContent = "This is the header title";
-//     console.log('this works333');
-    
-// }
+import Icon from '../candle-logo.jpg';
 
 export function changeSidebarTitle() {
-    const sidebarTitle = document.getElementById('sidebarTitle');
-    if (sidebarTitle) {
         const textDiv = document.createElement('div');
-        textDiv.textContent = "this is the sidebar Title";
-        console.log('this works5555');
+        textDiv.textContent = "Light Up Your Day, One Task at a Time";
+        textDiv.classList.add('sidebarText');
         sidebarTitle.appendChild(textDiv);
-        
-    }
 }
 
 export function changeSidebarDescText() {
-    const sidebarDescText = document.getElementById('sidebarDescText');
-    if (headerTitle) {
         const textDiv = document.createElement('div');
-        textDiv.textContent = "this is the sidebar Description";
-        console.log('this works777');
+        textDiv.textContent = "What do you need to do?";
         sidebarDescText.appendChild(textDiv);
-    }
 }
 
-export function changeHeaderLogo() {
-    const headerLogo = document.getElementById('headerLogo');
-    if (headerLogo) {
-        const textDiv = document.createElement('div');
-        textDiv.textContent = 'This is the updated header logo';
-        headerLogo.appendChild(textDiv);
-    }
+export function changeHeader() {
+    const myIconContainer = document.createElement('div');
+    myIconContainer.classList.add('myIconContainer');
+    
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    headerLogo.appendChild(myIconContainer)
+    myIconContainer.appendChild(myIcon);
+    
+    const brandDiv = document.createElement('div');
+    brandDiv.textContent = 'Lavender List';
+    headerBrand.appendChild(brandDiv);
+    
+    const titleDiv = document.createElement('div');
+    titleDiv.textContent = 'Relax & Achieve';
+    headerTitle.appendChild(titleDiv);
 }
 
-export function changeHeaderTitle() {
-    const headerTitle = document.getElementById('headerTitle');
-    if (headerTitle) {
-        const textDiv = document.createElement('div');
-        textDiv.textContent = 'This is the updated header title';
-        headerTitle.appendChild(textDiv);
-    }
+export function changeFormText() {
+    const taskInput = document.querySelector('#Task');
+    taskInput.placeholder = 'Enter Task Title';
+
+    const descInput = document.querySelector('#Description');
+    descInput.placeholder = 'Add a Description';
+
+    const dueInput = document.querySelector('#Due');
+    dueInput.placeholder = 'Tonight, 6/8, 10 minutes';
 }

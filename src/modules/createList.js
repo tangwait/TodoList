013cb27f6981a6a14255aa7ea1094d3b4888db42
tangwai <tmichael11314@gmail.com>
2023-11-03 02:28:41 -0500

    add toggle/delete button add changetext functions add local storage on page load

--- src/modules/createList.js
+++ src/modules/createList.js
@@ -1,36 +1,42 @@
+import { saveData, getData } from './storage.js';
 
 const myList = [];
 
 export class List {
-    constructor(name, desc, due) {
-        this.name = name,
+    constructor(task, desc, due) {
+        this.task = task,
         this.desc = desc,
         this.due = due;
     }
 }
 
 export function addToMyList() {
-    const listNameInput = document.querySelector('#name').value;
-    const listDescInput = document.querySelector('#description').value;
-    const listDueInput = document.querySelector('#due').value;
+    const listTaskInput = document.querySelector('#Task').value;
+    const listDescInput = document.querySelector('#Description').value;
+    const listDueInput = document.querySelector('#Due').value;
 
-    if (listNameInput && listDescInput && listDueInput) {
-        const newList = new List(listNameInput, listDescInput, listDueInput);
+    if (listTaskInput && listDescInput && listDueInput) {
+        const newList = new List(listTaskInput, listDescInput, listDueInput);
         myList.push(newList);
 
-        document.querySelector('#name').value = '';
-        document.querySelector('#description').value = '';
-        document.querySelector('#due').value = '';
+        document.querySelector('#Task').value = '';
+        document.querySelector('#Description').value = '';
+        document.querySelector('#Due').value = '';
 
-        console.log('listNameInput:', listNameInput);
+        console.log('listTaskInput:', listTaskInput);
         console.log('listDescInput:', listDescInput);
         console.log('listDueInput:', listDueInput);
         
         const newIndex = myList.length - 1; 
         makeAndAppendCard(newIndex);
-        makeDeleteButton(newIndex);
-        console.log(myList);
+     
+        
+        changeTaskText(listTaskInput, newIndex);
+        changeDescText(listDescInput, newIndex);
+        changeDueText(listDueInput, newIndex);
     }
+
+    saveData('myTaskList', myList);
 }
 
 
@@ -39,35 +45,36 @@ export function makeAndAppendCard(index) {
 
     const listCard = document.createElement('div');
     listCard.classList.add(`card${index}`);
+    listCard.classList.add('incomplete');
     listCard.id = "listCard";
 
-    const listDetails = ['Name', 'Desc', 'Due'];
+    const listDetails = ['Task', 'Desc', 'Due'];
 
     listDetails.forEach(detail => {
         const nameElement = document.createElement('div');
-        nameElement.classList.add('listCardItem');
+        nameElement.classList.add('listCardItems');
+        nameElement.id = (`listCard-${detail}`);
         nameElement.textContent = `${detail}: ${myList[index][detail.toLowerCase()]}`;
 
         listCard.appendChild(nameElement);
     });
+    const parentElement = listCard;
+    const buttonDiv = createAppendButtonDiv(parentElement);
+    makeToggleButton(index, buttonDiv);
+    makeDeleteButton(index, buttonDiv);
 
     mainContentContainer.appendChild(listCard);
-    console.log("make and append card works");
 }
 
-
-function makeDeleteButton(index) {
+function makeDeleteButton(index, buttonDiv) {
     const deleteButton = document.createElement ('button');
-    deleteButton.textContent = 'Delete';
+    deleteButton.textContent = 'Extinguish';
     deleteButton.classList.add('deleteButton');
     deleteButton.addEventListener('click', () => {
         deleteList(index)
         removeCardDOM(index)
     });
-    
-    const listCard = document.querySelector(`.card${index}`)
-    listCard.appendChild(deleteButton);
-    console.log("this works")
+    buttonDiv.appendChild(deleteButton);
 }
 
 function removeCardDOM(index) {
@@ -79,5 +86,80 @@ function deleteList(index) {
 
 }
 
+function makeToggleButton(index, buttonDiv) {
+    const toggleTaskButton = document.createElement('button');
+    toggleTaskButton.textContent = 'Completed';
+    toggleTaskButton.classList.add('taskButton');
+    toggleTaskButton.addEventListener('click', () => toggleTask(index));
+
+    buttonDiv.appendChild(toggleTaskButton);
+
+}
+
+function toggleTask(index) {
+    const listCards = document.querySelectorAll(`.card${index}`);
+
+    listCards.forEach(listCard => {
+        // Toggle background color
+        listCard.style.backgroundColor = listCard.classList.contains("completed") ? "#FFDAB9" : "#D8BFD8";
+
+        // Toggle the 'completed' and 'incomplete' classes
+        if (listCard.classList.contains("completed")) {
+            listCard.classList.remove("completed");
+            listCard.classList.add("incomplete");
+        } else if (listCard.classList.contains("incomplete")) {
+            listCard.classList.remove("incomplete");
+            listCard.classList.add("completed");
+        } 
+    });
+}
 
 
+
+
+function createAppendButtonDiv(parentElement) {
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('buttonDiv');
+
+    
+    parentElement.appendChild(buttonDiv);
+    return buttonDiv;
+}
+
+function changeTaskText(listTaskInput, index) {
+    const taskDiv = document.createElement('div');
+    taskDiv.textContent = listTaskInput;
+    taskDiv.classList.add('taskContentDiv');
+
+    const listCardTask = document.querySelector(`.card${index} #listCard-Task`); 
+    listCardTask.textContent = '';
+    listCardTask.appendChild(taskDiv);
+}
+
+function changeDescText(listDescInput, index) {
+    const descDiv = document.createElement('div');
+    descDiv.textContent = listDescInput;
+    descDiv.classList.add('descContentDiv');
+
+    const listCardDesc = document.querySelector(`.card${index} #listCard-Desc`); 
+    listCardDesc.textContent = '';
+    listCardDesc.appendChild(descDiv);
+}
+
+function changeDueText(listDueInput, index) {
+    const dueDiv = document.createElement('div');
+    dueDiv.textContent = listDueInput;
+    dueDiv.classList.add('dueContentDiv');
+
+    const listCardDue = document.querySelector(`.card${index} #listCard-Due`); 
+    listCardDue.textContent = '';
+    listCardDue.appendChild(dueDiv);
+}
+
+window.addEventListener('load', () => {
+    const storedTasks = getData('myTaskList');
+    if (storedTasks) {
+        myList.length = 0;
+        myList.push(...storedTasks); 
+      }
+});

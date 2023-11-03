197be02fd184833d44ec9c8cdc169fe59e9b7e9f
tangwai <tmichael11314@gmail.com>
2023-11-03 01:51:44 -0500

    delete test.js update packages update webpack.config > add icon + date library

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const { supportedLocales } = require('./config.js'); 
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Tangwait's Todo List'",
+      title: "Tangwait's Todo List", 
     }),
+    new webpack.ContextReplacementPlugin(
+      /date-fns[/\\]locale/,
+      new RegExp(`\\.[/\\\\](${supportedLocales.join('|')})[/\\\\]index\\.js$`)
+    ),
   ],
   output: {
     filename: 'bundle.js',
@@ -19,6 +26,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },

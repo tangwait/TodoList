0b055a839ce43c39663e5f237b149c53a4fc5081
tangwai <tmichael11314@gmail.com>
2023-11-08 14:33:40 -0600

    fix .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Tangwait's Todo List'",
+      title: "Tangwait's Todo List",
     }),
   ],
   output: {

ec68d3308fa29b31740f4bb991b1471771bac862
tangwai <tmichael11314@gmail.com>
2023-11-08 13:59:34 -0600

    delete webpack library config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-
-const { supportedLocales } = require('./config.js'); 
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Tangwait's Todo List", 
+      title: "Tangwait's Todo List'",
     }),
-    new webpack.ContextReplacementPlugin(
-      /date-fns[/\\]locale/,
-      new RegExp(`\\.[/\\\\](${supportedLocales.join('|')})[/\\\\]index\\.js$`)
-    ),
   ],
   output: {
     filename: 'bundle.js',
